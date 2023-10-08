9d814d8dbe2eb854f1e59cf80aea0597a9490729
Kaian Samuel <kaiansamuelster@gmail.com>
2023-10-08 18:16:45 -0300

    Cadeia Criptográfica

--- protochain/__tests__/block.test.ts
+++ protochain/__tests__/block.test.ts
@@ -1,43 +1,50 @@
-import { describe, test, expect } from '@jest/globals';
+import { describe, test, expect, beforeAll } from '@jest/globals';
 import Block from '../src/lib/block';
 
 describe("Block tests", () => {
 
+  let genesis: Block;
+
+  beforeAll(() => {
+    genesis = new Block(0, '', 'Genesis Block')
+  })
+
   test("Should be valid", () => {
-    const block = new Block(1, 'abc', 'Block 2');
-    const valid = block.isValid();
+    const block = new Block(1, genesis.hash, 'Block 2');
+    const valid = block.isValid(genesis.hash, genesis.index);
     expect(valid).toBeTruthy();
   })
 
   test("Should not be valid(previous hash)", () => {
-    const block = new Block(1, '', 'Block 2');
-    const valid = block.isValid();
+    const block = new Block(1, 'abc', 'Block 2');
+    const valid = block.isValid(genesis.hash, genesis.index);
     expect(valid).toBeFalsy();
   })
 
   test("Should not be valid(timestamp)", () => {
-    const block = new Block(1, 'abc', 'Block 2');
+    const block = new Block(1, genesis.hash, 'Block 2');
     block.timestamp = -1;
-    const valid = block.isValid();
+    block.hash = block.getHash()
+    const valid = block.isValid(genesis.hash, genesis.index);
     expect(valid).toBeFalsy();
   })
 
   test("Should not be valid(hash)", () => {
-    const block = new Block(1, 'abc', 'Block 2');
+    const block = new Block(1, genesis.hash, 'Block 2');
     block.hash = '';
-    const valid = block.isValid();
+    const valid = block.isValid(genesis.hash, genesis.index);
     expect(valid).toBeFalsy();
   })
 
   test("Should not be valid(data)", () => {
-    const block = new Block(1, 'abc', '');
-    const valid = block.isValid();
+    const block = new Block(1, genesis.hash, '');
+    const valid = block.isValid(genesis.hash, genesis.index);
     expect(valid).toBeFalsy();
   })
 
   test("Should not be valid(index)", () => {
-    const block = new Block(-1, 'abc', 'Block 2');
-    const valid = block.isValid();
+    const block = new Block(-1, genesis.hash, 'Block 2');
+    const valid = block.isValid(genesis.hash, genesis.index);
     expect(valid).toBeFalsy();
   })
 
--- protochain/__tests__/blockchain.test.ts
+++ protochain/__tests__/blockchain.test.ts
@@ -1,11 +1,44 @@
 import { describe, test, expect } from '@jest/globals';
+import Block from '../src/lib/block'
 import Blockchain from '../src/lib/blockchain';
 
 describe("Blockchain tests", () => {
 
-  test("Should be valid", () => {
+  test("Should has genesis blocks", () => {
     const blockchain = new Blockchain();
     expect(blockchain.blocks.length).toEqual(1);
+  })
+  
+  test("Should be valid (genesis)", () => {
+    const blockchain = new Blockchain();
+    expect(blockchain.isValid()).toEqual(true);
+  })
+  
+  test("Should be valid (two blocks)", () => {
+    const blockchain = new Blockchain();
+    blockchain.addBlock(new Block(1, blockchain.blocks[0].hash, 'Block 2'))
+    expect(blockchain.isValid()).toEqual(true);
+  })
+  
+  test("Should not be valid", () => {
+    const blockchain = new Blockchain();
+    blockchain.addBlock(new Block(1, blockchain.blocks[0].hash, 'Block 2'))
+    blockchain.blocks[1].data = 'a transfere 2 para b'
+    expect(blockchain.isValid()).toEqual(false);
+  })
+  
+  test("Should add block", () => {
+    const blockchain = new Blockchain();
+    const result = 
+    blockchain.addBlock(new Block(1, blockchain.blocks[0].hash, 'Block 2'))
+    expect(result).toEqual(true);
+  })
+  
+  test("Should not add block", () => {
+    const blockchain = new Blockchain();
+    const block = new Block(-1, blockchain.blocks[0].hash, 'Block 2')
+    const result = blockchain.addBlock(block);
+    expect(result).toEqual(false);
   })
 
  
